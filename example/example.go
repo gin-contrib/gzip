cdf45de67e0e4068834b96d56b9f547e3d0d172e
Hristo Trendev <htrendev@gmail.com>
2022-12-14 15:53:48 +0000

    add support for server-sent events (fixes #67)

--- example/example.go
+++ example/example.go
@@ -16,6 +16,15 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
+	r.GET("/stream", func(c *gin.Context) {
+		c.Header("Content-Type", "text/event-stream")
+		c.Header("Connection", "keep-alive")
+		for i := 0; i < 10; i++ {
+			fmt.Fprintf(c.Writer, "id: %d\ndata: tick %d\n\n", i, time.Now().Unix())
+			c.Writer.Flush()
+			time.Sleep(1 * time.Second)
+		}
+	})
 
 	// Listen and Server in 0.0.0.0:8080
 	if err := r.Run(":8080"); err != nil {
